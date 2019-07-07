afe2823d5109ed482ee9aeaa733e0831e8d2518b
yy05853 <c05853yy@gmail.com>
2019-07-07 13:51:35 +0900

    list3-18,19

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,20 +1,26 @@
 package com.tuyano.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HeloController {
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num, ModelAndView mav) {
-		int res = 0;
-		for(int i = 1; i <=num; i++) {
-			res += i;
-		}
-		mav.addObject("msg", "total: " + res);
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "お名前を書いて送信してください");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(@RequestParam("text1")String str,
+			ModelAndView mav) {
+		mav.addObject("msg","こんにちは、" + str + "さん！");
+		mav.addObject("value",str);
 		mav.setViewName("index");
 		return mav;
 	}
