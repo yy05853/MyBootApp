24bb8b8cb8f524314af965113c1f0ff886d65f2a
yy05853 <c05853yy@gmail.com>
2019-07-07 13:10:46 +0900

    list3-14

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,50 +1,14 @@
 package com.tuyano.springboot;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HeloController {
 
-	String[] names = {"tuyano",
-			"hanako","taro",
-			"sachiko", "ichiro"};
-	String[] mails = {"syoda@tuuyano.com",
-			"hanako@flower","taro@yamda",
-			"sachiko@happy", "ichiro@baseball"};
-
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id) {
-		return new DataObject(id, names[id], mails[id]);
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
-
-
-}
-
-class DataObject {
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-
-	}
-
-	public int getId() {return id;}
-
-	public void setId(int id) { this.id = id;}
-
-	public String getName() {return name ;}
-
-	public void setName(String name) {this.name = name;}
-
-	public String getValue() {return value;}
-
-	public void setValue(String value) {this.value = value;}
 
 }
