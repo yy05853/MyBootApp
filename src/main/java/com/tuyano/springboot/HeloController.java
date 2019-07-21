9935eb435582271d7892fa9e5c527c9bd57ec3a5
yy05853 <c05853yy@gmail.com>
2019-07-21 14:33:44 +0900

    list4-23,4-24

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -2,6 +2,7 @@ package com.tuyano.springboot;
 
 import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,14 +16,21 @@ public class HeloController {
 			"hanako@flower","taro@yamda",
 			"sachiko@happy", "ichiro@baseball"};
 
-	@RequestMapping("/")
-	public ModelAndView index(ModelAndView mav) {
+	@RequestMapping("/{num}")
+	public ModelAndView index(@PathVariable int num,
+			ModelAndView mav) {
 		mav.setViewName("index");
-		ArrayList<String[]> data = new ArrayList<String[]>();
-		data.add(new String[] {"taro","taro@yamada","090-999-999"});
-		data.add(new String[] {"hanako","hanako@flower","080-888-888"});
-		data.add(new String[] {"sachiko","sachiko@happy","080-888-888"});
-
+		mav.addObject("num",num);
+		if(num >= 0) {
+			mav.addObject("check","num >= data.size() ? 0 : num");
+		} else {
+			mav.addObject("check","num <= data.size() * -1 ? 0 : num * -1");
+		}
+		ArrayList<DataObject> data = new ArrayList<DataObject>();
+		data.add(new DataObject(0,"taro","taro@yamada"));
+		data.add(new DataObject(1,"hanako","hanako@flower"));
+		data.add(new DataObject(2,"sachiko","sachiko@happy"));
+		
 		mav.addObject("data",data);
 		return mav;
 	}
