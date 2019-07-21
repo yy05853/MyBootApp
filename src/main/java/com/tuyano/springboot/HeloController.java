70a89d7ff3e7faea7bd905a5875610fd4e40c1aa
yy05853 <c05853yy@gmail.com>
2019-07-21 14:47:55 +0900

    list4-25,26

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -2,7 +2,6 @@ package com.tuyano.springboot;
 
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,21 +15,14 @@ public class HeloController {
 			"hanako@flower","taro@yamda",
 			"sachiko@happy", "ichiro@baseball"};
 
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num,
-			ModelAndView mav) {
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("index");
-		mav.addObject("num",num);
-		if(num >= 0) {
-			mav.addObject("check","num >= data.size() ? 0 : num");
-		} else {
-			mav.addObject("check","num <= data.size() * -1 ? 0 : num * -1");
-		}
 		ArrayList<DataObject> data = new ArrayList<DataObject>();
 		data.add(new DataObject(0,"taro","taro@yamada"));
 		data.add(new DataObject(1,"hanako","hanako@flower"));
 		data.add(new DataObject(2,"sachiko","sachiko@happy"));
-		
+
 		mav.addObject("data",data);
 		return mav;
 	}
