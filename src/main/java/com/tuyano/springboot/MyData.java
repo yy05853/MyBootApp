24493ba26c0a6548fe520b5207ff8e5b99bf1a8f
yy05853 <c05853yy@gmail.com>
2019-08-04 15:21:40 +0900

    list5-25

--- src/main/java/com/tuyano/springboot/MyData.java
+++ src/main/java/com/tuyano/springboot/MyData.java
@@ -24,16 +24,16 @@ public class MyData {
 	private long id;
 
 	@Column(length = 50, nullable = false)
-	@NotEmpty
+	@NotEmpty(message="空白は不可")
 	private String name;
 
 	@Column(length = 200, nullable = true)
-	@Email
+	@Email(message="メールアドレスのみ")
 	private String mail;
 
 	@Column(nullable = true)
-	@Min(0)
-	@Max(200)
+	@Min(value=0, message="ゼロ以上")
+	@Max(value=200, message="200以下")
 	private Integer age;
 
 	@Column(nullable = true)
