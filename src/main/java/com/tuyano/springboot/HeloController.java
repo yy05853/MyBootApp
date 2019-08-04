98b80cb3e48bfc8f4144a7d650029f2b564c76ac
yy05853 <c05853yy@gmail.com>
2019-08-04 14:23:09 +0900

    list5-18,19

--- src/main/java/com/tuyano/springboot/HeloController.java
+++ src/main/java/com/tuyano/springboot/HeloController.java
@@ -1,13 +1,17 @@
 package com.tuyano.springboot;
 
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tuyano.springboot.repositories.MyDataRepository;
@@ -63,6 +67,43 @@ public class HeloController {
 		repository.saveAndFlush(d3);
 
 	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView edit(@ModelAttribute MyData mydata,
+			@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("edit");
+		mav.addObject("title","edit mydata.");
+		Optional<MyData> data = repository.findById((long)id);
+		mav.addObject("formModel",data.get());
+		return mav;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView update(@ModelAttribute MyData mydata,
+			ModelAndView mav) {
+		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView delete(@PathVariable int id,
+			ModelAndView mav) {
+		mav.setViewName("delete");
+		mav.addObject("title","delete mydata.");
+		Optional<MyData> data = repository.findById((long)id);
+		mav.addObject("formModel", data.get());
+		return mav;
+	}
+
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView remove(@RequestParam long id,
+			ModelAndView mav) {
+		repository.deleteById(id);
+		return new ModelAndView("redirect:/");
+	}
+
 
 }
 
--- src/main/java/com/tuyano/springboot/repositories/MyDataRepository.java
+++ src/main/java/com/tuyano/springboot/repositories/MyDataRepository.java
@@ -1,5 +1,7 @@
 package com.tuyano.springboot.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.tuyano.springboot.MyData;
 
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long>{
+	public Optional<MyData> findById(Long name);
 
 }
